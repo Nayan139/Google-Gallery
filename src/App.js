9677477533aadec6cc9ab495f70fdb5bfca4f5c1
Nayan <nayanbhakhar7075@gmail.com>
2022-12-03 22:31:38 +0530

    Deploy issue fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Box, Container } from "@mui/material";
+import { Container } from "@mui/material";
 import Home from "./Components/Home/Home";
 import Navbar from "./Components/Navbar/Navbar";
 import "./App.css";
