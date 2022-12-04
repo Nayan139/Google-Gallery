18328de20fae916510b370da106e34bebc28cb48
Nayan <nayanbhakhar7075@gmail.com>
2022-12-04 23:33:38 +0530

    Image Upload feature added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,19 +1,61 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Typography } from "@mui/material";
 import { BsFillPatchPlusFill } from "react-icons/bs";
+import { storage } from "../../auth/config";
+import { listAll, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import "./Home.css";
 
 const Home = () => {
-  const [error, setError] = useState(false);
+  const [error, setError] = useState({ flag: false, msg: "" });
+  const [data, setData] = useState([]);
+
   const Files = ["image/jpg", "image/jpeg", "image/png"];
-  const handleFileUpload = (e) => {
-    const file = e.target.files;
-    if (file.length > 0 && !Files.includes(file[0].type)) {
-      setError(true);
-    } else {
-      setError(false);
-    }
+  const listRef = ref(storage, "/images");
+
+  useEffect(() => {
+    getImageList();
+  }, []);
+
+  const getImageList = () => {
+    listAll(listRef).then((res) => {
+      res.items.forEach((item) =>
+        getDownloadURL(item).then((url) => {
+          setData([...data, { downloadURL: url }]);
+        })
+      );
+    });
   };
+
+  const handleFileUpload = async (e) => {
+    try {
+      const file = e.target.files;
+      if (file.length > 0 && !Files.includes(file[0].type)) {
+        setError({
+          msg: " Please upload files in .jpeg .jpg .png format",
+          flag: true,
+        });
+      } else {
+        setError({ msg: "", flag: false });
+
+        const metadata = {
+          contentType: "image/jpeg",
+        };
+        const storageRef = ref(storage, `/images/${file[0].name}`, metadata);
+
+        uploadBytes(storageRef, file[0], metadata).then((snapshot) => {
+          setError({ msg: "File Uploaded Successfully", flag: false });
+          handleSetRest();
+        });
+      }
+    } catch (error) {}
+  };
+
+  const handleSetRest = () => {
+    setTimeout(() => {
+      setError({ msg: "", error: false });
+    }, 2000);
+  };
+
   return (
     <Box className="Box_home">
       <Box sx={{ display: "flex", flexDirection: "column" }}>
@@ -46,20 +88,21 @@ const Home = () => {
         <label htmlFor="icon-button-file" style={{ placeSelf: "center" }}>
           <BsFillPatchPlusFill className="upload_icon" />
         </label>
-        {error ? (
-          <Typography
-            sx={{
-              fontFamily: "PlayfairDisplay",
-              fontSize: "1rem",
-              color: "#de6b6b",
-              placeSelf: "center",
-            }}
-          >
-            Please upload files in .jpeg .jpg .png format
-          </Typography>
-        ) : (
-          ""
-        )}
+        <Typography
+          sx={{
+            fontFamily: "PlayfairDisplay",
+            fontSize: "1rem",
+            color: error.flag ? "#de6b6b" : "rgb(51, 179, 199)",
+            placeSelf: "center",
+          }}
+        >
+          {error.flag ? error.msg : error.msg}
+        </Typography>
+        {data.length > 0
+          ? data.map((items) => (
+              <img src={`${items.downloadURL}.png`} alt="name" />
+            ))
+          : ""}
       </Box>
     </Box>
   );
--- src/auth/config.js
+++ src/auth/config.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,5 +21,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
-export const storage = app.storage();
+const db = getFirestore(app);
+const storage = getStorage(app);
+const analytics = getAnalytics(app);
+
+export { storage, analytics, app };
+
+export default db;
